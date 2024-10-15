26af56640e1e71d6cc060c38502d54eb0a4d75a0
SupertrampCode <vad23y@gmail.com>
2024-10-15 22:40:59 +0300

    max tokens field in ChatCompletionRequest.java ->restrict size of gpt response

--- src/main/java/com/petproject/aipet_project/ai/config/AIConfigs.java
+++ src/main/java/com/petproject/aipet_project/ai/config/AIConfigs.java
@@ -11,4 +11,7 @@ public class AIConfigs {
 
     @Value("${spring.ai.openai.address}")
     public static String url;
+
+    @Value(value = "{spring.ai.openai.chat.options.max-tokens}")
+    public static int max_tokens;
 }
